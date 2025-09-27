2d2e7dd375f0e3c130edfff9f7cd8c897156d58a
Augusto <senescal.oliveira@gmail.com>
2025-09-27 11:12:03 -0300

    feat: adicao do js do projeto

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+// selecao de elementos 
+
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+const multiplicationTable = document.querySelector("#multiplication-operations")
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
+// funcoes
+
+const createTable = (number, multiplicatorNumber) => {
+
+    multiplicationTable.innerHTML = "";
+
+    for(i = 1;  i <= multiplicatorNumber; i++){
+
+        const result  = number * i;
+
+        const template = `<div class="row">
+            <div class=operation>${number} x ${i} = </div>
+            <div class="result"> ${result}</div>
+        </div>`;
+
+        const parser = new DOMParser()
+
+        const htmlTemplate = parser.parseFromString(template,"text/html")
+
+        const row = htmlTemplate.querySelector(".row")
+
+        multiplicationTable.appendChild(row);
+    }
+
+    multiplicationTitle.innerText = number;
+}
+
+// eventos
+
+multiplicationForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    // Validação para ver se ambos os numeros não são nulos
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber);
+})
